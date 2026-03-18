c79b83998a9bb0e172699e36e858c81fc90c94cf
RadoslawDrab <radoslaw.drab03@gmail.com>
2026-03-18 12:24:24 +0100

    ``` feat(server): enhance main.py for improved routing and deployment (#123)
    - Added dynamic routing for serving static files and index.html.
    - Integrated Waitress for production-ready server handling.
    - Enhanced logging to indicate server mode (development/preview/production).
    - Updated server initialization to support custom host and port.
    - Streamlined code structure for better readability and maintainability.
    ```

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
+import sys
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
+from waitress import serve
 
+import tasks
 from api import api, downloads, data
-from utils import Status, get_env
+from utils import Status
 from utils.logger import Logger, LoggerFormat
 from utils.tasks import TasksHandler
 from utils.args import Args
@@ -24,6 +27,14 @@ app.route("/api", methods=["GET", "POST"])(api)
 app.route("/api/<string:artist>/<string:title>", methods=["GET"])(data)
 app.route("/audio/<path:path>", methods=["GET"])(downloads)
 
+@app.route('/', defaults={ 'path': '' })
+@app.route('/<path:path>')
+def _(path):
+	full_path = Path(app.static_folder).joinpath(path)
+	if path and full_path.exists():
+		return send_from_directory(app.static_folder, path, as_attachment=True)
+	return send_from_directory(app.static_folder, 'index.html')
+
 # Configure logging
 Logger(
 	Args.log_path,
@@ -35,7 +46,26 @@ Logger(
 def init():
 	TasksHandler.set_kwargs(app=app, output_path=Args.output_path, temp_path=Args.temp_path)
 	TasksHandler.start()
-	app.run(port=3001, debug=True)
+
+	Logger.log(f'MODE: { "development" if Args.dev and not Args.preview else ("preview" if Args.preview else "production") }', log_type='DEBUG')
+
+	if not Args.dev or Args.preview:
+		Logger.log(f'Server started on http://{Args.host}:{Args.port}', log_type='INFO')
+
+		serve(
+			app,
+			host=Args.host,
+			port=Args.port,
+			threads=12,
+			connection_limit=200,
+			channel_timeout=60
+        )
+	else:
+		app.run(
+			port=Args.port,
+			debug=True,
+			host=Args.host
+		)
 
 if __name__ == "__main__":
 	init()
