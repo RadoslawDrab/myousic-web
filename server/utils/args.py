9f8b146b0b996c5b3718b3adbed3a2f35eb3481f
RadoslawDrab <radoslaw.drab03@gmail.com>
2026-03-23 14:03:13 +0100

    ``` feat(args): enhance argument parsing and configuration options (#123)
    - Updated output_path, db_path, and log_path to use environment variables.
    - Introduced log_level argument for customizable logging verbosity.
    - Added db-path argument for specifying the audio database folder.
    - Enhanced job_workers parameter for improved asynchronous downloads.
    - Clarified argument descriptions for better user understanding.
    ```

--- server/utils/args.py
+++ server/utils/args.py
@@ -9,12 +9,14 @@ PREVIEW = get_env('PREVIEW', default=False, type=bool)
 class Args(Singleton):
 	dev: bool = not PREVIEW
 	preview: bool = PREVIEW
-	output_path: Path = Path('./.preview/audio/' if PREVIEW else get_env('SERVER_OUTPUT_PATH', default='./audio', type=Path))
-	temp_path: Path = Path('./tmp')
-	log_path: Path = Path('./.preview/file.log' if PREVIEW else './.tmp/file.log')
+	output_path: Path = get_env('SERVER_OUTPUT_PATH', default='./.preview/audio/' if PREVIEW else './audio', type=Path)
+	db_path: Path = get_env('SERVER_DB_PATH', default='./.preview' if PREVIEW else './', type=Path)
+	log_path: Path = get_env('SERVER_LOG_PATH', default='./.preview/file.log' if PREVIEW else './.tmp/file.log', type=Path)
+	log_level: str = get_env('SERVER_LOG_LEVEL', default='INFO' if PREVIEW else 'DEBUG')
 	port: int = 3001
 	host: str = 'localhost'
 	app_path: Path = Path.cwd().parent.joinpath('client/dist') if PREVIEW else Path.cwd().joinpath('client')
+	job_workers: int = 3
 
 	def __init__(self) -> None:
 		super().__init__()
@@ -25,10 +27,12 @@ class Args(Singleton):
 		parser.add_argument('--prod', help='Mark as production', action='store_false', dest='dev', default=not self.dev)
 		parser.add_argument('--port', '-p', help='Server port', type=int, default=self.port)
 		parser.add_argument('--host', help='Server host', type=str, default=self.host)
-		parser.add_argument('--output-path', '-o', help='Audio folder', type=Path, default=self.output_path)
-		parser.add_argument('--temp-path', help='Audio temporary folder. Relative to `output-path`', type=Path, default=self.temp_path)
+		parser.add_argument('--output-path', '-o', help='Audio download folder', type=Path, default=self.output_path)
+		parser.add_argument('--db-path', help='Audio DB folder', type=Path, default=self.db_path)
 		parser.add_argument('--log-path', help='Log file path', type=Path, default=self.log_path)
+		parser.add_argument('--log-level', help='Log level. Available [\'DEBUG\', \'INFO\', \'WARNING\', \'ERROR\']', default=self.log_level, choices=['DEBUG', 'INFO',' WARNING', 'ERROR'])
 		parser.add_argument('--app-path', help='Client app folder path', type=Path, default=self.app_path)
+		parser.add_argument('--job-workers', '-w', help='How many workers should download asynchronously. More workers = faster downloads', type=int, default=self.job_workers)
 
 		args = parser.parse_args()
 
